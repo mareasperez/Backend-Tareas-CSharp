2124532637cb6b0307279b88351b782330479142
Maury Areas <mareasperez7@gmail.com>
2021-04-01 13:15:14 -0600

    Agregar archivos de proyecto.

---
+++ Tareas-Backend-Api/Context/AplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tareas_Backend_Api.models;
+
+namespace Tareas_Backend_Api.Context
+{
+    public class AplicationDbContext : DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options): base(options)
+        {
+
+        }
+        public DbSet<Tarea> Tareas { get; set; }
+    }
+}
---
+++ Tareas-Backend-Api/Controllers/TareaController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tareas_Backend_Api.Context;
+
+namespace Tareas_Backend_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TareaController : ControllerBase
+    {
+        private readonly AplicationDbContext _context;
+        public TareaController(AplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var listTareas = await _context.Tareas.ToListAsync();
+                return Ok(listTareas);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
